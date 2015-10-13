a6dec3a15f2c56bba9a8f3fe5400347b7c9ccb30
shink <shink_7770@qq.com>
2015-10-13 17:15:50 +0800

    基础代码及参考资料

---
+++ Run.rb
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+require_relative '../SCRF/加载'
+require_relative '代码/加载'
+
+忽略错误 do
+  
+  #~ 执行所有测试集
+end
---
+++ 代码/加载.rb
@@ -0,0 +1,8 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+根路径 = File.dirname(__FILE__)
+
+忽略错误 do
+  注册模块('FuncRuby', 根路径, true)
+end
+
