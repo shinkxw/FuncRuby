716172b7ed8a8ed12b12957f621a23f396813ac3
shink <shink_7770@qq.com>
2015-10-15 00:03:17 +0800

    代码上传

---
+++ 代码/FR常量库/动态常量.rb
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+class Object
+  def self.常量缺失(常量名)
+    输出 常量名
+    #丘奇数
+    
+    #字符串
+    
+    引发 NameError, "undefined constant #{常量名}"
+  end
+end
