f35d6b1d36e2dcbc1d363619be463684dddd60aa
shink <shink_7770@qq.com>
2015-10-15 15:43:55 +0800

    代码更新

--- Run.rb
+++ Run.rb
@@ -6,8 +6,8 @@ require_relative '代码/加载'
 忽略错误 do
   FR2 = FR递增[FR1]
   FR3 = FR递增[FR2]
-  输出 FR.数值(FR加[FR_4][FR1])
-  输出 FR.数值(FR乘[FR_4][FR1])
+  输出 FR.数值(FR加[FRN_4][FR1])
+  输出 FR.数值(FR乘[FRS_4][FR1])
   输出 FR.数值(FR乘方[FR3][FR3])
   #~ 执行所有测试集
 end
--- 代码/FR常量库/动态常量.rb
+++ 代码/FR常量库/动态常量.rb
@@ -2,11 +2,11 @@
 # encoding: UTF-8
 class Object
   def self.常量缺失(常量名)
-    输出 常量名
+    常量字符串 = 常量名.字符串化
     #丘奇数
-    
+    输出 'num' if 常量字符串.开始于?('FRN_')
     #字符串
-    
+    输出 'str' if 常量字符串.开始于?('FRS_')
     引发 NameError, "undefined constant #{常量名}"
   end
 end
