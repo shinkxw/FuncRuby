e6c5dfc0b24f985fac0f44d6f368bac46147b051
shink <shink_7770@qq.com>
2015-10-18 23:54:56 +0800

    更新代码

--- Run.rb
+++ Run.rb
@@ -4,10 +4,19 @@ require_relative '../SCRF/加载'
 require_relative '代码/加载'
 
 忽略错误 do
-  FR2 = FR递增[FRN_1]
-  FR3 = FR递增[FRN_2]
-  输出 FR.数值(FR加[FRN_937][FRN_2])
-  输出 FR.数值(FR乘[FRN_40][FRN_4])
-  输出 FR.数值(FR乘方[FRN_3][FRN_6])
+  结果集 = FR列表收集[FR范围[FRN_1][FRN_100]][-> n {
+    FR如果[FR为零[FR取余[n][FRN_15]]][
+      'FizzBuzz'
+    ][FR如果[FR为零[FR取余[n][FRN_3]]][
+      'Fizz'
+    ][FR如果[FR为零[FR取余[n][FRN_5]]][
+      'Buzz'
+    ][
+      n.to_s
+    ]]]
+  }]
+  FR列表叠加[结果集][FR空列表][-> l { -> 结果 { 输出 结果 } }]
+  #~ 输出 FR.数值(FR列表头[结果集])
+  输出 FR列表头[FR列表剩余[FR列表剩余[结果集]]]
   #~ 执行所有测试集
 end
---
+++ 代码/FR常量库/组合子.rb
@@ -0,0 +1,6 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+#Y(f(x)) 等价于 f(x)
+FRY组合子 = -> f { -> x { f[x[x]] }[-> x { f[x[x]] }] }
+#Y组合子 + 延迟运算
+FRZ组合子 = -> f { -> x { f[-> y { x[x][y] }] }[-> x { f[-> y { x[x][y] }] }] }
