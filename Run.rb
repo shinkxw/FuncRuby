67c5553f9869facbe5ff27d72b9943a9b2fa0db1
shink <shink_7770@qq.com>
2015-10-19 23:14:36 +0800

    代码更新

--- Run.rb
+++ Run.rb
@@ -6,17 +6,16 @@ require_relative '代码/加载'
 忽略错误 do
   结果集 = FR列表收集[FR范围[FRN_1][FRN_100]][-> n {
     FR如果[FR为零[FR取余[n][FRN_15]]][
-      'FizzBuzz'
+      FRS_FizzBuzz
     ][FR如果[FR为零[FR取余[n][FRN_3]]][
-      'Fizz'
+      FRS_Fizz
     ][FR如果[FR为零[FR取余[n][FRN_5]]][
-      'Buzz'
+      FRS_Buzz
     ][
       n.to_s
     ]]]
   }]
-  FR列表叠加[结果集][FR空列表][-> l { -> 结果 { 输出 结果 } }]
-  #~ 输出 FR.数值(FR列表头[结果集])
-  输出 FR列表头[FR列表剩余[FR列表剩余[结果集]]]
+  输出 FR.字符串(FR列表头[FR列表剩余[FR列表剩余[结果集]]])
+  输出 FR.字符串(FRS_我爱你)
   #~ 执行所有测试集
 end
--- 代码/FR常量库/动态常量.rb
+++ 代码/FR常量库/动态常量.rb
@@ -3,10 +3,10 @@
 class Object
   def self.常量缺失(常量名)
     常量字符串 = 常量名.字符串化
-    if /^FR(?<类型>.)_(?<值>\d+)$/ =~ 常量字符串
+    if /^FR(?<类型>.)_(?<值>.+)$/ =~ 常量字符串
       常量值 = case 类型
                when 'N' then FR.丘奇数化(值.整数化)
-               when 'S' then FR.丘奇串化(值)
+               when 'S' then FR.λ字符串化(值)
                else 引发 NameError, "undefined constant #{常量名}"
                end
       定义常量(常量名, 常量值)
