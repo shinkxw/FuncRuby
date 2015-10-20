c5f27f0e9d234d507c0149e2e093d96ee1f5ec32
shink <shink_7770@qq.com>
2015-10-20 23:55:26 +0800

    代码更新

--- Run.rb
+++ Run.rb
@@ -4,7 +4,7 @@ require_relative '../SCRF/加载'
 require_relative '代码/加载'
 
 忽略错误 do
-  结果集 = FR列表收集[FR范围[FRN_1][FRN_100]][-> n {
+  结果集 = FR列表收集[FR范围[FRN_1][FRN_50]][-> n {
     FR如果[FR为零[FR取余[n][FRN_15]]][
       FRS_FizzBuzz
     ][FR如果[FR为零[FR取余[n][FRN_3]]][
@@ -12,10 +12,8 @@ require_relative '代码/加载'
     ][FR如果[FR为零[FR取余[n][FRN_5]]][
       FRS_Buzz
     ][
-      n.to_s
+      FR数值转字符串[n]
     ]]]
   }]
-  输出 FR.字符串(FR列表头[FR列表剩余[FR列表剩余[结果集]]])
-  输出 FR.字符串(FRS_我爱你)
-  #~ 执行所有测试集
+  FR.数组(结果集).每个{|结果| 输出 FR.字符串(结果)}
 end
